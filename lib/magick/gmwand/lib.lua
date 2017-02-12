b77b616ac0a893b467edd384ab28c72ccd6389a7
Luo <luoxianmingg@gmail.com>
2017-02-12 16:05:36 +0800

    init repository

---
+++ lib/magick/gmwand/lib.lua
@@ -0,0 +1,38 @@
+local ffi = require("ffi")
+ffi.cdef([[  void InitializeMagick( const char *path );
+  typedef void MagickWand;
+  typedef unsigned int MagickPassFail;
+  typedef int ExceptionType;
+
+  typedef enum {
+    UndefinedFilter
+  } FilterTypes;
+
+  MagickWand *NewMagickWand();
+  MagickPassFail MagickReadImage(MagickWand *,const char *);
+  MagickPassFail MagickReadImageBlob(MagickWand *,const unsigned char *,const size_t length);
+  MagickPassFail MagickWriteImage(MagickWand *,const char *);
+
+  unsigned char *MagickWriteImageBlob(MagickWand *,size_t *);
+
+  MagickPassFail DestroyMagickWand(MagickWand *);
+  MagickPassFail MagickRelinquishMemory(void *);
+
+  char* MagickGetException(const MagickWand*, ExceptionType*);
+
+  unsigned long MagickGetImageHeight(MagickWand *);
+  unsigned long MagickGetImageWidth(MagickWand *);
+
+  MagickPassFail MagickScaleImage(MagickWand *,const unsigned long,const unsigned long);
+  MagickPassFail MagickResizeImage(MagickWand *,const unsigned long,const unsigned long, const FilterTypes,const double);
+  MagickPassFail MagickCropImage(MagickWand *,const unsigned long,const unsigned long, const long,const long);
+
+  MagickPassFail MagickSetImageFormat(MagickWand* wand, const char* format);
+  char* MagickGetImageFormat(MagickWand* wand);
+  MagickPassFail MagickSetCompressionQuality(MagickWand *wand,
+  const size_t quality);
+]])
+local gmwand = ffi.load("GraphicsMagickWand")
+return {
+  lib = gmwand
+}
---
+++ lua/test.lua
@@ -0,0 +1,5 @@
+
+local file = io.open("/data/pictrix/tmp/1268660595130086541479691665089","wb")
+file:write("hllxkis")
+file:close()
+
